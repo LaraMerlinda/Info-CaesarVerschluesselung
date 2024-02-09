b25580f7a5d473e218fb97591f5f6f9ec5585e74
S210894 <lara@mayet.net>
2024-02-09 11:16:16 +0100

    euzjnue

--- CaesarSimple.java
+++ CaesarSimple.java
@@ -7,7 +7,16 @@ public class CaesarSimple
     
     public void Entschluesseln(String satz, int schluessel)
     {
-        
+        System.out.print("Die Lösung ist: ");
+        for(int i = 0; i < satz.length(); i++)
+        {
+            char a = satz.charAt(i);
+            int code = (int) a;
+            code = code - schluessel;
+            char zeichen = (char) code;
+            System.out.print(zeichen);
+        }
+        System.out.println("");
     }
     
     public void Teste()
@@ -17,6 +26,7 @@ public class CaesarSimple
     
     public void Verschluessle(String satz, int schluessel)
     {
+        System.out.print("Die Lösung ist: ");
         for(int i = 0; i < satz.length(); i++)
         {
             char a = satz.charAt(i);
@@ -25,13 +35,15 @@ public class CaesarSimple
             char zeichen = (char) code;
             System.out.print(zeichen);
         }
+        System.out.println("");
     }
     
     public void VerschiebeBuchstabe(char ch, int abstand)
     {
-        for (int i = 0; i <; i++) {
-
-        }
+        int code = (int) ch;
+        code = code + abstand;
+        char zeichen = (char) code;
+        System.out.print(zeichen);
     }
     
     public void VerschiebeSatz(String satz, int abstand)
